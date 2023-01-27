e084c5ecbbf692f762f73fe4bdd3b7e354925583
Evgeniy1984No <norboev.eugeny@yandex.ru>
2023-01-27 17:11:50 +1100

    Add the 13 and 14 tasks in the Matrixes

--- Leetcode/For tests.py
+++ Leetcode/For tests.py
@@ -1,12 +1,12 @@
-my_list = [[12, 221, 3], [41, 5, 633], [71, 8, 99]]
-
-maximum = my_list[0][0]
-minimum = my_list[0][0]
-
-for row in my_list:
-    if max(row) > maximum:
-        maximum = max(row)
-    if min(row) < minimum:
-        minimum = min(row)
-
-print(maximum + minimum)
+desk = [['.'] * 8 for _ in range(8)]
+col_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+print(desk)
+[print(*row) for row in desk]
+print()
+n = input()
+i = ~int(n[1])+1
+j = col_dict[n[0]]
+desk[i][j] = 'N'
+[print(*row) for row in desk]
+print(i)
+print(i+11 in range(8))
