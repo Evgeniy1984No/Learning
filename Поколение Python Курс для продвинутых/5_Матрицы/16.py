7ea79127c67ca6ef8f1dc9dd034cdc3132ed0856
Evgeniy1984No <norboev.eugeny@yandex.ru>
2023-02-12 17:10:49 +1100

    Add the 16 task in the Matrixes

--- Поколение Python Курс для продвинутых/5_Матрицы/16.py
+++ Поколение Python Курс для продвинутых/5_Матрицы/16.py
@@ -53,4 +53,15 @@ for i in range(n):
         if i % 2 != 0 and (j == 0 or j % 2 == 0):
             desk[i][j] = '*'
     print(*desk[i])
+"""
+_____________________________________________________________________________________________________________________
+"""
+n, m = [int(i) for i in input().split()]
+board = [['.'] * m for _ in range(n)]
 
+for i in range(n):
+    for j in range(1 - i % 2, m, 2):
+        board[i][j] = '*'
+
+for row in board:
+    print(*row)
