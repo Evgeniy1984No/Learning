9bc51095464e005e1b2bb459845b741f436cc58f
Evgeniy1984No <norboev.eugeny@yandex.ru>
2023-02-13 11:04:16 +1100

    Add the best solution in the 20 task of Matrixes

--- Поколение Python Курс для продвинутых/5_Матрицы/20.py
+++ Поколение Python Курс для продвинутых/5_Матрицы/20.py
@@ -57,3 +57,13 @@ for i in range(n):
         if i == j or j == n - i - 1:
             mat[i][j] = 1
 [print(*[str(j).ljust(3) for j in i], sep='') for i in mat]
+
+"""
+__________________________________________________________________________________________________________________
+"""
+n = int(input())
+
+res = [[1 if i == j or i == n - j - 1 else 0 for j in range(n)] for i in range(n)]
+
+for x in res:
+    print(*x)
