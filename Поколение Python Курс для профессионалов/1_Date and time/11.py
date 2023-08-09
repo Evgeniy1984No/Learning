a34411391b1fe4d34f35a58093fd818ff5b62a3f
Evgeniy1984No <norboev.eugeny@yandex.ru>
2023-08-09 16:36:10 +1100

    Add tasks 10-13 -> 1_Date and time

---
+++ Поколение Python Курс для профессионалов/1_Date and time/11.py
@@ -0,0 +1,11 @@
+"""
+Дополните приведенный ниже код, чтобы он преобразовал секунды seconds (прошедшие от начала эпохи) в объект datetime и,
+наоборот, объект datetime в секунды (прошедшие от начала эпохи).
+"""
+from datetime import datetime
+
+seconds = 2483228800
+dt = datetime(2011, 11, 4)
+
+print(datetime.fromtimestamp(seconds))
+print(dt.timestamp())
